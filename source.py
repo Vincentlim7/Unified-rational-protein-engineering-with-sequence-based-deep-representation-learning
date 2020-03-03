8b1c5ae0c593950e640196acaf369d05c229a0db
Vincentlim7 <limvincent246@gmail.com>
2020-03-03 18:33:20 +0100

    unused code deleted

--- source.py
+++ source.py
@@ -61,10 +61,6 @@ b = babbler(batch_size=batch_size, model_path=MODEL_WEIGHT_PATH)
 
 
 from scipy.spatial import distance
-# PLUS BESOIN DE get_rd_prot() JE CROIS
-def get_rd_prot():    # Retourne une ligne aleatoire du fichier fullProtein.list
-    lines = open("fullProtein.list").read().splitlines()
-    return np.random.choice(lines).split() # return ("protein name", "protein class)
 
 def get_prot_seq(file_name):
     f = open("dataset/fastas/" + file_name + ".fasta", "r")
@@ -184,65 +180,4 @@ for key, value in classes.items():
 prot1 = classes["a.1.1.2"]["d1sctb_"]
 prot2 = classes["a.1.1.4"]["d1kr7a_"]
 print(distance.euclidean(prot1, prot2))
-
-
-# In[59]:
-
-
-from scipy.spatial import distance
-
-f = open("fullProtein.list")
-
-line = next(f).split()
-studied_prot = get_seq(line[0])
-
-same_class = []
-rd_class = []
-
-nb_seq_class = 5
-
-
-for i in range (nb_seq_class): #Recuperer les noms de fichiers
-    line = next(f).split()
-    same_class.append(line[0])
-    rd_protein =random_line("fullProtein.list")
-    rd_class.append(line[0])
-    
-f.close
-
-print(same_class)
-print(rd_class)
-
-seq_list1 = [] # Recuperer les sequences
-dist1 = []
-for i in range(nb_seq_class):
-    prot = get_seq(same_class[0])
-    seq_list1.append(prot)
-    #dist1.append(distance.euclidean(studied_prot[1], prot[1]))
-
-seq_list2 = []
-dist2 = []
-for i in range(nb_seq_class):
-    prot = get_seq(rd_class[0])
-    seq_list2.append(prot)
-    #dist2.append(distance.euclidean(studied_prot[1], prot[1]))
-
-print(seq_list1)
-
-"""rd_protein = random_line("fullProtein.list")
-print(random_protein)
-
-file_name = "dataset/fastas/" + protein[0] + ".fasta"
-rd_file_name = "dataset/fastas/" + rd_protein[0] + ".fasta"
-
-seq, seq_vector = get_seq(file_name)
-rd_seq, rd_seq_vector = get_seq(rd_file_name)
-
-print("Studied sequence :")
-print(seq + "\n")
-print("Sequence size : " + str(len(seq)) + "\n")
-print("Vector representation of the sequence :\n" + str(seq_vector) + "\n")
-print("Vector size : " + str(len(seq_vector)) + "\n")
-print("Is seq a valide sequence ?\n" + str(b.is_valid_seq(seq)) + "\n")
-"""
 
