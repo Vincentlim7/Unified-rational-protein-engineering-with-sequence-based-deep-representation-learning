e244295d8143cc254a419cf910587dac3a71f4dc
Vincentlim7 <limvincent246@gmail.com>
2020-03-03 17:50:25 +0100

    dist_extra finished

--- source.py
+++ source.py
@@ -57,7 +57,7 @@ batch_size = 12
 b = babbler(batch_size=batch_size, model_path=MODEL_WEIGHT_PATH)
 
 
-# In[4]:
+# In[60]:
 
 
 from scipy.spatial import distance
@@ -109,7 +109,7 @@ def dic_init(avg = True):
             classes[classe][protein] = get_concat_vec(get_prot_seq(protein))
     return classes
 
-def dist_intra(protein_dict): # Fonctionne 
+def get_dist_intra(protein_dict): # Fonctionne 
     dist_intra = dict()
     for classe, protein_list in protein_dict.items():
         if classe not in dist_intra:
@@ -124,23 +124,26 @@ def dist_intra(protein_dict): # Fonctionne
                     dist_intra[classe][protein_a] = (protein_b, dist)
     return dist_intra
 
-def dist_extra(protein_dict): # A CODER 
+def get_dist_extra(protein_dict): # A CODER 
     dist_extra = dict()
-    for classe, protein_list in protein_dict.items():
-        for protein_a, seq_a in protein_list.items():
-            dist_extra[protein_a] = (None, np.inf)
-            for protein_b, seq_b in protein_list.items():
-                if protein_a == protein_b:
+    for classe_a, protein_list_a in protein_dict.items():
+        if classe_a not in dist_extra:
+            dist_extra[classe_a] = dict()
+        for protein_a, vec_a in protein_list_a.items():
+            dist_extra[classe_a][protein_a] = (None, np.inf)
+            for classe_b, protein_list_b in protein_dict.items():
+                if classe_a == classe_b:
                     continue
-                dist = distance.euclidean(seq_a, seq_b)
-                if dist < dist_extra[protein_a][1]:
-                    dist_extra[protein_a] = (protein_b, dist)
+                for protein_b, vec_b in protein_list_b.items():
+                    dist = distance.euclidean(vec_a, vec_b)
+                    if dist < dist_extra[classe_a][protein_a][1]:
+                        dist_extra[classe_a][protein_a] = (protein_b, dist)
     return dist_extra
                     
                 
 
 
-# In[5]:
+# In[61]:
 
 
 # EXEMPLE VECTEURS
@@ -149,29 +152,38 @@ print(get_avg_vec(seq))
 print(get_concat_vec(seq))
 
 
-# In[6]:
+# In[62]:
 
 
 classes = dic_init()
 print(classes)
 
 
-# In[7]:
+# In[64]:
 
 
-# EXEMPLE DISTANCE EUCLIDIENNE
-prot_a = classes["a.1.1.4"]["d1kr7a_"]
-prot_b = classes["a.1.1.1"]["d1ngka_"]
-print(prot_a)
-print(prot_b)
-print(distance.euclidean(prot_a, prot_b))
-
-
-# In[8]:
-
-
-dist_intra = dist_intra(classes)
+dist_intra = get_dist_intra(classes)
 print(dist_intra)
+
+
+# In[65]:
+
+
+dist_extra = get_dist_extra(classes)
+print(dist_extra)
+
+
+# In[66]:
+
+
+# Testing manually if dist intra/extra are correct
+for key, value in classes.items():
+    print(key)
+    for key2, value2, in value.items():
+        print("  ", key2)
+prot1 = classes["a.1.1.2"]["d1sctb_"]
+prot2 = classes["a.1.1.4"]["d1kr7a_"]
+print(distance.euclidean(prot1, prot2))
 
 
 # In[59]:
