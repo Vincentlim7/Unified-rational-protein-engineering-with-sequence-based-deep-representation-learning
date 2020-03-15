ee71a2c1e1de32c69a1ea0f4116354aec1e91f96
Vincentlim7 <limvincent246@gmail.com>
2020-03-15 01:45:35 +0100

    9630 proteins processed

--- source.py
+++ source.py
@@ -212,8 +212,8 @@ print(total_elapsed_time)
 # In[ ]:
 
 
-classes_avg = np.load("database/data_avg8931.npy")[()]
-classes_concat = np.load("database/data_concat8931.npy")[()]
+classes_avg = np.load("database/data_avg9630.npy")[()]
+classes_concat = np.load("database/data_concat9630.npy")[()]
 
 
 # In[ ]:
@@ -246,7 +246,7 @@ histo(dist_intra_concat, dist_extra_concat)
 
 
 cpt = 0
-for classe, protein_list in classes_avg.items():
+for classe, protein_list in classes_avg2.items():
     for protein_a, vec_a in protein_list.items():
         cpt += 1
 print(cpt)
@@ -255,14 +255,14 @@ print(cpt)
 # In[ ]:
 
 
-classes_avg2 = np.load("database/next_batch/data_avg900.npy")[()]
-classes_concat2 = np.load("database/next_batch/data_concat900.npy")[()]
+classes_avg2 = np.load("database/next_batch/data_avg700.npy")[()]
+classes_concat2 = np.load("database/next_batch/data_concat700.npy")[()]
 
 
 # In[ ]:
 
 
-del classes_concat2["c.37.1.8"]
+del classes_avg2["c.51.4.2"]
 
 
 # In[ ]:
@@ -275,12 +275,12 @@ classes_concat.update(classes_concat2)
 # In[ ]:
 
 
-np.save("database/data_avg8931.npy", classes_avg)
-np.save("database/data_concat8931.npy", classes_concat)
+np.save("database/data_avg9630.npy", classes_avg)
+np.save("database/data_concat9630.npy", classes_concat)
 
 
 # In[ ]:
 
 
-print(classes_avg2["c.37.1.7"])
+print(classes_avg["c.51.4.1"])
 
