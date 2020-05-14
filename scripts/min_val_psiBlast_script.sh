264517bcb60029943b2002ac5827d14711d742f9
Vincentlim7 <limvincent246@gmail.com>
2020-05-14 20:38:59 +0200

    min val script fix

--- scripts/min_val_psiBlast_script.sh
+++ scripts/min_val_psiBlast_script.sh
@@ -1,7 +1,7 @@
 for filename in ../dataset/psiblast_res/* ; 
 do 
 	FILE=$(echo $filename | rev | cut -d'/' -f1 | rev) ; 
-	sort -k 11 -g $filename  | head -n1 > ../dataset/noob/$FILE;
+	sort -k 11 -g $filename  | head -n1 > ../dataset/min_val_psiblast_res/$FILE;
 	
 done
 
